fd53bf93a000303c2a6bfbc192fd8ab0d117e53c
crossoverJie <crossoverjie@gmail.com>
2017-12-04 22:57:29 +0800

    :white_check_mark: Reverse Integer

---
+++ src/main/java/com/crossoverjie/leetcode/ReverseInteger.java
@@ -0,0 +1,26 @@
+package com.crossoverjie.leetcode;
+
+/**
+ * Function: 交换整数 Input: 123  Output:  321
+ *
+ * @author crossoverJie
+ *         Date: 04/12/2017 22:10
+ * @since JDK 1.8
+ */
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        long result = 0 ;
+
+        for (; x !=0 ; x = x / 10){
+            result = result * 10 + x % 10 ;
+        }
+
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE){
+            return 0 ;
+        }
+
+        return (int) result;
+    }
+
+}
---
+++ src/test/java/com/crossoverjie/leetcode/ReverseIntegerTest.java
@@ -0,0 +1,41 @@
+package com.crossoverjie.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReverseIntegerTest {
+
+    ReverseInteger reverseInteger = new ReverseInteger() ;
+
+    @Before
+    public void setUp() throws Exception {
+        reverseInteger = new ReverseInteger() ;
+    }
+
+    @Test
+    public void reverse() throws Exception {
+        int reverse = reverseInteger.reverse(9000);
+        Assert.assertEquals(reverse,9);
+    }
+
+
+    @Test
+    public void reverse2() throws Exception {
+        int reverse = reverseInteger.reverse(1123456789);
+        Assert.assertEquals(reverse,0);
+    }
+
+    @Test
+    public void reverse3() throws Exception {
+        int reverse = reverseInteger.reverse(-1123456789);
+        Assert.assertEquals(reverse,0);
+    }
+
+    @Test
+    public void reverse4() throws Exception {
+        int reverse = reverseInteger.reverse(9834);
+        Assert.assertEquals(reverse,4389);
+    }
+
+}
