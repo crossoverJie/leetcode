1d11fa96e1e1007e78d7c54f6e98e33fc0907ed4
crossoverJie <crossoverjie@gmail.com>
2017-12-10 02:21:51 +0800

    :sparkles: Merge Two Sort

---
+++ src/main/java/com/crossoverjie/leetcode/easy/ListNode.java
@@ -0,0 +1,17 @@
+package com.crossoverjie.leetcode.easy;
+
+/**
+ * Function:
+ *
+ * @author crossoverJie
+ *         Date: 07/12/2017 17:53
+ * @since JDK 1.8
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/crossoverjie/leetcode/easy/MergeTwoSortedLists.java
@@ -0,0 +1,16 @@
+package com.crossoverjie.leetcode.easy;
+
+/**
+ * Function:
+ *
+ * @author crossoverJie
+ *         Date: 07/12/2017 13:58
+ * @since JDK 1.8
+ */
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null ;
+    }
+
+}
