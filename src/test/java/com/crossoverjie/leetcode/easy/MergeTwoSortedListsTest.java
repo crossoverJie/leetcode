26d98392c8de222c4f4866b45b9e74c568a39a36
crossoverJie <crossoverjie@gmail.com>
2017-12-13 01:09:02 +0800

    :white_check_mark: test

--- src/test/java/com/crossoverjie/leetcode/easy/MergeTwoSortedListsTest.java
+++ src/test/java/com/crossoverjie/leetcode/easy/MergeTwoSortedListsTest.java
@@ -1,5 +1,6 @@
 package com.crossoverjie.leetcode.easy;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,7 +28,21 @@ public class MergeTwoSortedListsTest {
         l2_3.next = l2_4 ;
         ListNode listNode = mergeTwoSortedLists.mergeTwoLists(l1, l2);
 
-        System.out.println(listNode.toString());
+
+        ListNode node1 = new ListNode(1) ;
+        ListNode node2 = new ListNode(1);
+        node1.next = node2;
+        ListNode node3 = new ListNode(3) ;
+        node2.next= node3 ;
+        ListNode node4 = new ListNode(4) ;
+        node3.next = node4 ;
+        ListNode node5 = new ListNode(5) ;
+        node4.next = node5 ;
+        ListNode node6 = new ListNode(6) ;
+        node5.next = node6 ;
+        ListNode node7 = new ListNode(9) ;
+        node6.next = node7 ;
+        Assert.assertEquals(node1.toString(),listNode.toString());
 
 
     }
@@ -46,6 +61,31 @@ public class MergeTwoSortedListsTest {
 
         System.out.println(listNode.toString());
 
+
+    }
+
+    @Test
+    public void mergeTwoLists3() throws Exception {
+
+        ListNode l2 = new ListNode(1) ;
+        ListNode l2_2 = new ListNode(3) ;
+        l2.next = l2_2 ;
+        ListNode l2_3 = new ListNode(6) ;
+        l2_2.next = l2_3 ;
+        ListNode l2_4 = new ListNode(9) ;
+        l2_3.next = l2_4 ;
+        ListNode listNode = mergeTwoSortedLists.mergeTwoLists(l2, null);
+
+
+        ListNode node1 = new ListNode(1) ;
+        ListNode node2 = new ListNode(3);
+        node1.next = node2;
+        ListNode node3 = new ListNode(6) ;
+        node2.next= node3 ;
+        ListNode node4 = new ListNode(9) ;
+        node3.next = node4 ;
+
+        Assert.assertEquals(node1.toString(),listNode.toString());
 
     }
 
