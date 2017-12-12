f1786796269c15edfc311e5df8ff08ce1b0d3dbf
crossoverJie <crossoverjie@gmail.com>
2017-12-12 22:57:10 +0800

    :memo: comment

--- src/main/java/com/crossoverjie/leetcode/easy/MergeTwoSortedLists.java
+++ src/main/java/com/crossoverjie/leetcode/easy/MergeTwoSortedLists.java
@@ -10,6 +10,14 @@ package com.crossoverjie.leetcode.easy;
 public class MergeTwoSortedLists {
 
 
+    /**
+     * 1.声明一个头结点
+     * 2.将头结点的应用赋值给一个临时结点，也可以叫做下一结点。
+     * 3.
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0) ;
         ListNode lastNode = head ;
