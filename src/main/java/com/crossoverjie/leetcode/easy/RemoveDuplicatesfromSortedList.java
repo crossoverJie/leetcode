51d107c2842a435a5dfabdec89d2322d79c55646
crossoverJie <crossoverjie@gmail.com>
2018-03-04 23:47:36 +0800

    :sparkles: Introducing new features.

---
+++ src/main/java/com/crossoverjie/leetcode/easy/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,11 @@
+package com.crossoverjie.leetcode.easy;
+
+/**
+ * Function: 删除链表中重复的元素
+ *
+ * @author crossoverJie
+ *         Date: 04/03/2018 23:47
+ * @since JDK 1.8
+ */
+public class RemoveDuplicatesfromSortedList {
+}
