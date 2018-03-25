76ef4f3268dd337c6b091c9020685b17f23e2e2f
crossoverJie <crossoverjie@gmail.com>
2018-03-25 23:30:10 +0800

    :sparkles: Introducing new features.Top K Frequent Elements

---
+++ src/main/java/com/crossoverjie/leetcode/medium/TopKFrequentElements.java
@@ -0,0 +1,12 @@
+package com.crossoverjie.leetcode.medium;
+
+/**
+ * Function: 出现频率最高的 K 个数
+ * Given [1,1,1,2,2,3] and k = 2, return [1,2].
+ *
+ * @author crossoverJie
+ *         Date: 25/03/2018 23:29
+ * @since JDK 1.8
+ */
+public class TopKFrequentElements {
+}
